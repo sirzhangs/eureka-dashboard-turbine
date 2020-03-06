8f0d89bb3d5bfc2f00309fa13c131ec226af0b23
sirzhangs <sirzhangs@163.com>
2020-03-06 11:13:53 +0800

    熔断监控

---
+++ src/test/java/com/sirzhangs/dashboard/turbine/EurekaDashboardTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirzhangs.dashboard.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDashboardTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
